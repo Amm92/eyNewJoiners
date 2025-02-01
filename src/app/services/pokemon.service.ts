178c4572123f90e4cc9aea36c2b7c3a2edb73833
Antonio José Martín Morcillo <ajmartin@externos.cecabank.es>
2025-02-02 00:54:29 +0100

    Ejecicio practico

---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environments';
+import { map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  private _apiurl = environment.API_URL;
+
+  constructor(private _http: HttpClient) { }
+
+  public getAll(page: number): Observable<any>{
+    if(page !== 0){
+      --page;
+    }
+
+    page = Math.max(0,page);
+
+    return this._http.get(`${this._apiurl}pokemon?offset=${page*20}&limit=20`).pipe(
+      map((resp:any) =>{
+        const simplePokemons: any[] = resp.results.map(
+          (pokemon:any) => ({
+            id:pokemon.url.split('/').at(-2) ?? '',
+            name: pokemon.name,
+            total: resp.count
+          })
+        );
+        return simplePokemons
+
+      })
+    );
+  }
+
+  public getPokemon(number:string): Observable<any>{
+    return this._http.get(`${this._apiurl}pokemon/${number}`);
+  }
+}
